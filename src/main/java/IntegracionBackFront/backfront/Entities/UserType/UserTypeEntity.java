7e23c3323aca2c34b121e90f150eeb9cb80bbba5
Astrid <20240235@ricaldone.edu.sv>
2025-09-02 11:25:17 -0600

    Cambios 11:25

---
+++ src/main/java/IntegracionBackFront/backfront/Entities/UserType/UserTypeEntity.java
@@ -0,0 +1,33 @@
+package IntegracionBackFront.backfront.Entities.UserType;
+
+import IntegracionBackFront.backfront.Entities.Users.UserEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode
+@Table(name = "TIPOUSUARIO")
+public class UserTypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_tipousuario")
+    @SequenceGenerator(name = "seq_tipousuario", sequenceName = "seq_tipousuario", allocationSize = 1)
+    @Column(name = "IDTIPOUSUARIO")
+    private Long id;
+
+    @Column(name = "NOMBRETIPO")
+    private String nombreTipo;
+
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+
+    @OneToMany(mappedBy = "tipoUsuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore //Que el campo no sea requerido
+    private List<UserEntity> usuarios = new ArrayList<>();
+}
